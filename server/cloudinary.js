9309930cf530096dc11b9b48686300d5e4204baa
ufimoni <ekongbert02@gmail.com>
2025-05-08 20:14:10 +0100

    cloudinary

---
+++ server/cloudinary.js
@@ -0,0 +1,8 @@
+const cloudinary = require('cloudinary');
+cloudinary.config({ 
+    cloud_name: 'dl88tovtn', 
+    api_key: '935483773956738', 
+    api_secret: '30EdhOxLz1c-5TEfN53nt7s3U-E'
+  });
+
+  module.exports = cloudinary;
