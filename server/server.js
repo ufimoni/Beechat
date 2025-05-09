069a45f049525e6047c7d9b8ab8771e96403bfee
Bertrand <136034307+ufimoni@users.noreply.github.com>
2025-05-09 16:50:00 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ dotenv.config({path: './config.env'})
 
 const dbconfig = require('./config/dbconfig');
 
-const server = require('./App');
+const server = require('./app');
 
 const port = process.env.port
 server.listen(port, ()=>{
