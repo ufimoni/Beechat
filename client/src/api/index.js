80c85628e7876f2057fe05d503d3f3650b69f43a
Bertrand <136034307+ufimoni@users.noreply.github.com>
2025-05-09 19:16:11 +0100

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const url = "http://localhost:5000";
+export const url = "https://beechat-server.onrender.com";
 /// creating the instance of the axios
 export const axiosInstance = axios.create({
     headers:{
