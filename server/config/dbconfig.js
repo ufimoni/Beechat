67e6e0d6c5b901b72fdfae0e8b029d9f5d279680
Bertrand <136034307+ufimoni@users.noreply.github.com>
2025-05-09 17:17:36 +0100

    Update dbconfig.js

--- server/config/dbconfig.js
+++ server/config/dbconfig.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
 
-//// Connection logic
-mongoose.connect(process.env.Conn_Str);
+// Connect to MongoDB
+mongoose.connect(process.env.Conn_Str, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-///Testing connection
+// Get the connection
 const db = mongoose.connection;
-/// listening to the connections coming.
-db.on('connected',()=>{
-    console.log("Database Connected Successfully!")
-})
- 
-/// checking for error
-db.on('err',()=>{
-console.log("Error in connecting to the database");
-})
+
+// Success
+db.on('connected', () => {
+  console.log('Database Connected Successfully!');
+});
+
+// Error
+db.on('error', (err) => {
+  console.error('Error in connecting to the database:', err);
+});
 
 module.exports = db;
