d64e3d28485634131c4bd22c990b6254861832cd
Bertrand <136034307+ufimoni@users.noreply.github.com>
2025-05-10 13:25:11 +0100

    Update index.js

--- client/src/pages/profile/index.js
+++ client/src/pages/profile/index.js
@@ -2,15 +2,14 @@
 import moment from 'moment';
 import {useEffect, useState} from 'react';
 import { useSelector } from 'react-redux';
-import { uploadProfilePic } from './../../apiCalls/user';
+import { uploadProfilePic } from '././../../api/user';
 import { useDispatch } from 'react-redux';
-import { hideLoader, showLoader } from '../../redux/loaderSlice';
+import { hideLoader, showLoader } from './../../redux/loaderSlice';
 import toast from 'react-hot-toast'
-<<<<<<< HEAD
+
 import { setUser } from './../../redux/userSlice';
-=======
+
 import { setUser } from './../../redux/usersSlice';
->>>>>>> bfed02845494ceeae854423f810864fd1a1822e6
 
 
 function Profile(){
